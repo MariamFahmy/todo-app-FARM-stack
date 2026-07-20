8cc966787d084a397f6ff483186b4e2def6119a1
MariamFahmy <mariamff11@gmail.com>
2026-07-20 11:29:35 -0230

    Defined methods for getting summaries of to-do lists and creating a new to-do list in the MongoDB database

--- backend/src/dal.py
+++ backend/src/dal.py
@@ -13,6 +13,7 @@ class ListSummary(BaseModel):
     item_count: int
 
     @staticmethod
+    # create instances from MongoDB documents
     def from_doc(doc) -> "ListSummary":
         return ListSummary(
             id=str(doc["_id"]),
@@ -46,3 +47,34 @@ class ToDoList(BaseModel):
             items=[ToDoListItem.from_doc(item) for item in doc["items"]],
         )
 
+class ToDoDAL:
+    """Encapsulate all database operations"""
+
+    def __init__(self, todo_collection: AsyncIOMotorCollection) {
+        self._todo_collection = todo_collection
+    }
+
+    async def list_todo_lists(self, session=None):
+        """Asynchronously yields summaries of all to-do lists from MongoDB database"""
+        async for doc in self._todo_collection.find(
+            {},
+            projection={
+                "name": 1,
+                "item_count": {"$size": "$items"},
+            },
+            sort={"name": 1},
+            session=session,
+        ):
+            yield ListSummary.from_doc(doc)
+
+    async def create_todo_list(self, name:str, session=None) -> str:
+        response = await self._todo_collection.insert_one(
+            {"name": name, "items": []},
+            session=session,
+        )
+        return str(response.inserted_id)
+
+    async def get_todo_list(self, id: str | ObjectId, session=None) -> ToDoList:
+        
+
+    
